db845624542c3462be9e4c158f655150e06f474d
chenniannian <chenniannian@rockontrol.com>
2021-01-14 14:13:58 +0800

    fix: 修复北大库使用方法错误

--- pku_test.go
+++ pku_test.go
@@ -1,6 +1,7 @@
 package crypto_benchmark
 
 import (
+	"bytes"
 	"github.com/Hyperledger-TWGC/pku-gm/gmssl"
 	"testing"
 )
@@ -18,56 +19,67 @@ func BenchmarkSign_PKU(b *testing.B) {
 	}
 }
 
-// bug太多 跑不同
-//func BenchmarkVerify_PKU(b *testing.B) {
-//	hashed := []byte("testing")
-//	sm2keygenargs := [][2]string{
-//		{"ec_paramgen_curve", "sm2p256v1"},
-//		{"ec_param_enc", "named_curve"},
-//	}
-//	priv, _ := gmssl.GeneratePrivateKey("EC", sm2keygenargs, nil)
-//	pk, _ := priv.GetPublicKey()
-//
-//	sm3hash := gmssl.New()
-//	sm3hash.Write(hashed)
-//	digest := sm3hash.Sum(nil)
-//	b.ResetTimer()
-//	sig,_ := priv.Sign("sm2sign", digest, nil)
-//	for i := 0; i < b.N; i++ {
-//		err := pk.Verify("sm2sign", digest,sig, nil)
-//		if err != nil{
-//			b.Errorf("failed to call pk.Verify, err:%v", err)
-//		}
-//
-//	}
-//}
-//
-//func TestEncAndDec_PKU(t *testing.T) {
-//	msg := []byte("sm2 encryption standard")
-//
-//	sm2keygenargs := [][2]string{
-//		{"ec_paramgen_curve", "sm2p256v1"},
-//		{"ec_param_enc", "named_curve"},
-//	}
-//	priv, _ := gmssl.GeneratePrivateKey("EC", sm2keygenargs, nil)
-//	pk, _ := priv.GetPublicKey()
-//
-//	//test encryption
-//	cipher, err :=  pk.Encrypt("sm2encrypt-with-sm3", msg, nil)
-//	if err != nil {
-//		t.Errorf("enc err:%s", err)
-//		return
-//	}
-//
-//	//test decryption
-//	plain, err := priv.Decrypt("sm2encrypt-with-sm3", cipher, nil)
-//	if err != nil {
-//		t.Errorf("dec err:%s", err)
-//		return
-//	}
-//
-//	if !bytes.Equal(msg, plain) {
-//		t.Error("sm2 encryption is invalid")
-//		return
-//	}
-//}
+func BenchmarkVerify_PKU(b *testing.B) {
+	hashed := []byte(TestHashString)
+	sm2keygenargs := [][2]string{
+		{"ec_paramgen_curve", "sm2p256v1"},
+		{"ec_param_enc", "named_curve"},
+	}
+	priv, _ := gmssl.GeneratePrivateKey("EC", sm2keygenargs, nil)
+	sm2pkpem, _ := priv.GetPublicKeyPEM()
+	pk, _ := gmssl.NewPublicKeyFromPEM(sm2pkpem)
+
+	sm3hash := gmssl.New()
+	sm3hash.Write(hashed)
+	digest := sm3hash.Sum(nil)
+	sig,_ := priv.Sign("sm2sign", digest, nil)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		err := pk.Verify("sm2sign", digest,sig, nil)
+		if err != nil{
+			b.Errorf("failed to call pk.Verify, err:%v", err)
+		}
+
+	}
+}
+
+func TestEncAndDec_PKU(t *testing.T) {
+	msg := []byte(TestHashString)
+
+	sm2keygenargs := [][2]string{
+		{"ec_paramgen_curve", "sm2p256v1"},
+		{"ec_param_enc", "named_curve"},
+	}
+	priv, _ := gmssl.GeneratePrivateKey("EC", sm2keygenargs, nil)
+	sm2pkpem, _ := priv.GetPublicKeyPEM()
+	pk, _ := gmssl.NewPublicKeyFromPEM(sm2pkpem)
+
+	//test encryption
+	cipher, err :=  pk.Encrypt("sm2encrypt-with-sm3", msg, nil)
+	if err != nil {
+		t.Errorf("enc err:%s", err)
+		return
+	}
+
+	//test decryption
+	plain, err := priv.Decrypt("sm2encrypt-with-sm3", cipher, nil)
+	if err != nil {
+		t.Errorf("dec err:%s", err)
+		return
+	}
+
+	if !bytes.Equal(msg, plain) {
+		t.Error("sm2 encryption is invalid")
+		return
+	}
+}
+
+func BenchmarkHash_PKU(b *testing.B){
+	origin := []byte(TestHashString)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		sm3hash := gmssl.New()
+		sm3hash.Write(origin)
+		  _ = sm3hash.Sum(nil)
+	}
+}
